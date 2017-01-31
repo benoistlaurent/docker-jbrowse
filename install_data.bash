d189096b3f7c7d11c23b2d834cc5e50b2291ca11
Benoist LAURENT <benoist.laurent@gmail.com>
2017-01-31 09:46:26 +0100

    corrected data installation paths

--- install_data.bash
+++ install_data.bash
@@ -5,10 +5,10 @@ rm -rf $JBROWSE_DATA/*
 
 mkdir -p $JBROWSE_DATA/raw/
 ln -s $DATA_DIR/cv11/ $JBROWSE_DATA/raw/cv11
-sed 's#"-dir" : "/data/cv11"#"-dir" : "'"$DATA_DIR"'"#' $DATA_DIR/cv11.json  > $JBROWSE_DATA/raw/
+sed 's#"-dir" : "/data/cv11"#"-dir" : "'"$DATA_DIR"'"#' $DATA_DIR/cv11.json  > $JBROWSE_DATA/raw/cv11.json
 
 prepare-refseqs.pl --fasta $DATA_DIR/cv11/cv11.fa --out $JBROWSE_DATA
-biodb-to-json.pl -v --conf $DATA_DIR/cv11.json --out $JBROWSE_DATA
+biodb-to-json.pl -v --conf $JBROWSE_DATA/raw/cv11.json --out $JBROWSE_DATA
 
 cat $DATA_DIR/cv11/directional_WTSS_RPM_strand+.bw.conf     >> $JBROWSE_DATA/tracks.conf
 cat $DATA_DIR/cv11/directional_WTSS_RPM_strand-.bw.conf     >> $JBROWSE_DATA/tracks.conf
