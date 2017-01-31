dc73d46326b4f22f79b554864cf9935856466de5
Benoist LAURENT <benoist.laurent@gmail.com>
2017-01-31 14:21:04 +0100

    replaced data/install_data.sh with Makefile

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,6 +4,6 @@ export JBROWSE_DATA=/jbrowse/data
 export JBROWSE=/jbrowse
 export DATA_DIR=/data
 
-[ -f $DATA_DIR/load.sh ] && . $DATA_DIR/load.sh
+[ -f $DATA_DIR/Makefile ] && make -f $DATA_DIR/Makefile
 
 nginx -g "daemon off;"
